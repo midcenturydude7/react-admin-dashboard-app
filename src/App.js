63a014cea8d2ab0dc13dacf36a294ae61f835519
midCenturyDude7 <mgriffes@gmail.com>
2023-04-17 17:17:32 -0700

    Completed darkmode settings

--- src/App.js
+++ src/App.js
@@ -28,10 +28,16 @@ import { useStateContext } from "./context/ContextProvider";
 import "./App.css";
 
 function App() {
-  const { activeMenu, themeSettings, setThemeSettings } = useStateContext();
+  const {
+    activeMenu,
+    themeSettings,
+    setThemeSettings,
+    currentColor,
+    currentMode,
+  } = useStateContext();
 
   return (
-    <div>
+    <div className={currentMode === "Dark" ? "dark" : ""}>
       <Router>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
@@ -39,7 +45,7 @@ function App() {
               <button
                 type="button"
                 className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                style={{ background: "blue", borderRadius: "50%" }}
+                style={{ background: currentColor, borderRadius: "50%" }}
                 onClick={() => {
                   setThemeSettings(true);
                 }}>
@@ -57,11 +63,9 @@ function App() {
             </div>
           )}
           <div
-            className={
-              activeMenu
-                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
-                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
-            }>
+            className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}>
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
               <Navbar />
             </div>
