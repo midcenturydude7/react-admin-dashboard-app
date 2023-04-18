3eae6825cadad8bcf382cddcc2bb4615b3d8ba2f
midCenturyDude7 <mgriffes@gmail.com>
2023-04-18 15:16:06 -0700

    Updated to account for unused vars

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 import { AiOutlineMenu } from "react-icons/ai";
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { BsCurrencyDollar } from "react-icons/bs";
 import { GoPrimitiveDot } from "react-icons/go";
